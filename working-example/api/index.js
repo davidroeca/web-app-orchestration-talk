d576633e11e587970170d97c0ad0f1da93ecf0ee
davidroeca <david.roeca@gmail.com>
2018-08-30 02:02:52 -0400

    migrate app to app1 and begin app2

--- working-example/api/index.js
+++ working-example/api/index.js
@@ -7,11 +7,11 @@ const server = http.createServer(app)
 
 const serverConfig = {
   host: '0.0.0.0',
-  port: 3000,
+  port: 5000,
 };
 
 const serverCallback = () => {
-  console.log('Listening on port 3000...');
+  console.log('Listening on port 5000...');
 };
 
 server.listen(serverConfig, serverCallback);
