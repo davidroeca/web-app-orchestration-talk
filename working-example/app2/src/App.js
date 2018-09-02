754544aea1b8b2dc6e236671bb2adeb3f6d7562f
davidroeca <david.roeca@gmail.com>
2018-09-02 15:57:45 -0400

    update styling for app2

--- working-example/app2/src/App.js
+++ working-example/app2/src/App.js
@@ -45,7 +45,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <a href='/app1'>Go to App 1</a>
+        <div>
+          <a href='/app1'>Go to App 1</a>
+        </div>
         {!error && items && items.length ? (
           <ol>
             {items.map((item, index) => (
